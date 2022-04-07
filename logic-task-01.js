39ec9ea81c8c87489784c8b5294c1020fe8d4390
Victoria Scholte <victoria.scholte@gmail.com>
2022-04-07 08:44:03 -0300

    add percentual variables in logic tasks 01 and 03.

--- logic-task-01.js
+++ logic-task-01.js
@@ -2,7 +2,8 @@
 // Exiba no console o valor do desconto e o preço a pagar.
 
 let valorMercadoria = 3000
-let desconto = valorMercadoria * 0.20
+let percentualDesconto = (25/100)
+let desconto = valorMercadoria * percentualDesconto
 let valorComDesconto = (valorMercadoria - desconto)
 
 console.log(`Você ganhou ${desconto} reais de desconto e o preço a pagar é ${valorComDesconto} reais.`)
--- logic-task-03.js
+++ logic-task-03.js
@@ -2,7 +2,8 @@
 // calcule e informe o valor da taxa do garçom (10%) e o valor total a ser pago.
 
 let valorJantar = 200
-let valorGorjeta = valorJantar * 0.10
+let percentualGorjeta = (10/100)
+let valorGorjeta = valorJantar * percentualGorjeta
 let valorJantarComGorjeta = valorJantar + valorGorjeta
 
-console.log(`O valor da gorjeta é ${valorGorjeta} reais e o total do Jantar é ${valorJantarComGorjeta} reais.`)
+console.log(`O valor da gorjeta é ${valorGorjeta} reais e o total a pagar é ${valorJantarComGorjeta} reais.`)
